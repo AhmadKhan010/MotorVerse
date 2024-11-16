dfcec96cc65e01efa9768a7254233604f615692f
Qusai80 <qusaimansoor00@gmail.com>
2024-11-16 15:37:15 +0500

    Packages are made

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.image.Image;
 
 public class Main extends Application {
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			Parent root = FXMLLoader.load(getClass().getResource("/views/Main.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/resource/Motoverse_Logo-removebg-preview.png")));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,11 @@
 module Motoverse {
 	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens views to javafx.fxml;
+    opens controllers to javafx.fxml;
+	exports controllers to javafx.fxml;
 }
